e443782eb67683a71f84bb819f67c25f288c03d7
Samik Mathur <fuydestroyer@gmail.com>
2022-12-21 14:46:58 -0700

    Pawn Attack

--- app/main.py
+++ app/main.py
@@ -2,30 +2,30 @@ import pygame
 
 pygame.init()
 
-
 (width, height) = (480, 480)
 screen = pygame.display.set_mode((width, height))
-for x in range(4):
-    Top = 120*x
-    for i in range(4):
-        Lefta = 120*i
-        Leftb = Lefta + 60
-        pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(Lefta, Top, 60, 60))
-        pygame.draw.rect(screen, (128, 76, 8), pygame.Rect(Leftb, Top, 60, 60))
-for x in range(4):
-    Top = 120*x+60
-    for i in range(4):
-        Lefta = 120*i
-        Leftb = Lefta + 60
-        pygame.draw.rect(screen, (128, 76, 8), pygame.Rect(Lefta, Top, 60, 60))
-        pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(Leftb, Top, 60, 60))
+
+for i in range(8):
+    for j in range(8):
+        if((i+j) % 2 == 0):
+            squareColor = (241, 212, 170)
+        else:
+            squareColor = (128, 76, 8)
+        pygame.draw.rect(screen, squareColor, pygame.Rect(i*60, j*60, 60, 60))
+
 img = pygame.image.load("C:\\Users\\SysC\\Pictures\\ChessPawn.png").convert()  
 img.set_colorkey((255, 255, 255))
-Pawn=(60,60)
-img = pygame.transform.scale(img, Pawn)
-screen.blit(img, (0, 60))     
-pygame.display.flip()
+img = pygame.transform.scale(img,(60,60))
+img2 = pygame.image.load("C:\\Users\\SysC\\Pictures\\ChessPawn2.png").convert()  
+img2.set_colorkey((0,0,0))
+img2 = pygame.transform.scale(img2, (60,60))
 
+for p in range(8):
+    screen.blit(img, (p*60, 60))
+    screen.blit(img2, (p*60, 360))
+
+
+pygame.display.flip()
 
 
 
