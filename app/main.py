3b12fb0b385e3f48398925a7c21a471236c1ee31
Samik Mathur <fuydestroyer@gmail.com>
2022-12-21 14:17:19 -0700

    pawn

--- app/main.py
+++ app/main.py
@@ -10,15 +10,20 @@ for x in range(4):
     for i in range(4):
         Lefta = 120*i
         Leftb = Lefta + 60
-        pygame.draw.rect(screen, (128, 76, 8), pygame.Rect(Lefta, Top, 60, 60))
-        pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(Leftb, Top, 60, 60))
+        pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(Lefta, Top, 60, 60))
+        pygame.draw.rect(screen, (128, 76, 8), pygame.Rect(Leftb, Top, 60, 60))
 for x in range(4):
     Top = 120*x+60
     for i in range(4):
         Lefta = 120*i
         Leftb = Lefta + 60
-        pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(Lefta, Top, 60, 60))
-        pygame.draw.rect(screen, (128, 76, 8), pygame.Rect(Leftb, Top, 60, 60))
+        pygame.draw.rect(screen, (128, 76, 8), pygame.Rect(Lefta, Top, 60, 60))
+        pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(Leftb, Top, 60, 60))
+img = pygame.image.load("C:\\Users\\SysC\\Pictures\\ChessPawn.png").convert()  
+img.set_colorkey((255, 255, 255))
+Pawn=(60,60)
+img = pygame.transform.scale(img, Pawn)
+screen.blit(img, (0, 60))     
 pygame.display.flip()
 
 
